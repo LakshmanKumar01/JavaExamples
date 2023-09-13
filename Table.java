a1541b4f43755293ebf38ec96f9e95ff5690ce33
LakshmanKumar01 <109958931+LakshmanKumar01@users.noreply.github.com>
2023-09-13 15:10:59 +0530

    Table.java

--- Table.java
+++ Table.java
@@ -1,7 +1,7 @@
 public class Table{
   public static void main(String[] args){
     int N = 2;
-    for(int i = 0; i <= 1; i++){
+    for(int i = 0; i <= 10; i++){
       System.out.println(N + "*" + i + "=" + N*i);
     }
   }
