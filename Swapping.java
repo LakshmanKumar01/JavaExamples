fd3e17db2bfdcc148ce6f3ba7c690e6001f80a9c
LakshmanKumar01 <109958931+LakshmanKumar01@users.noreply.github.com>
2023-11-25 18:35:33 +0530

    Update Swapping.java

--- Swapping.java
+++ Swapping.java
@@ -9,9 +9,9 @@ public class Swapping {
 		System.out.println("Before Swapping");
 		System.out.println("A value is " +a);
 		System.out.println("B value is " +b);
-		a = a+b;
-		b = a-b;
-		a = a-b;
+		a = a+b;       //int c = a
+		b = a-b;       //    a = b
+		a = a-b;       //    b = c	
 		System.out.println("After Swapping");
 		System.out.println("A value is " +a);
 		System.out.println("B value is " +b);
