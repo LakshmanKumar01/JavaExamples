e9c56823950b635807aca0bc114166508db93e4c
LakshmanKumar01 <109958931+LakshmanKumar01@users.noreply.github.com>
2023-12-08 11:54:06 +0530

    Update Swapping.java

--- Swapping.java
+++ Swapping.java
@@ -1,17 +1,19 @@
-package hello;
 
 public class Swapping {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		int a = 10;
 		int b = 30;
 		System.out.println("Before Swapping");
 		System.out.println("A value is " +a);
 		System.out.println("B value is " +b);
-		a = a+b;       //int c = a
-		b = a-b;       //    a = b
-		a = a-b;       //    b = c	
+
+                  
+                  // Using Third Variable
+	 a = a+b;        // int c = a
+		b = a-b;        //    a = b
+		a = a-b;        //    b = c	
 		System.out.println("After Swapping");
 		System.out.println("A value is " +a);
 		System.out.println("B value is " +b);
